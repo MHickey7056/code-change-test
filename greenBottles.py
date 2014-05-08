adf7e9478c6c5a15e3a1c12f0559ab69740ca3ff
MHickey7056 <mhickey7056@sohamvc.org.uk>
2014-05-08 12:02:54 +0100

    Update greenBottles.py
    Added time gaps between bottles text, used for x in range to print the green bottles text 10 times.

--- greenBottles.py
+++ greenBottles.py
@@ -1,4 +1,11 @@
-print("10 green bottles, hanging on the wall")
-print("10 green bottles, hanging on the wall")
-print("And if 1 green bottle should acidentally fall,")
-print("They'll be 9 green bottles hanging on the wall.")
+import time
+global b
+def bottles():
+    b=10
+    for x in range(10):
+        print(b, "green bottles, hanging on the wall.")
+        print(b, "green bottles, hanging on the wall.")
+        print("And if 1 green bottle should acidentally fall,")
+        b=b-1
+        print("There will be" ,b, "green bottles hanging on the wall.")
+        time.sleep(2)
