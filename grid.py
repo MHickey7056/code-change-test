a69c0e58727985a9bc6c45add780be829d4faea6
MHickey7056 <mhickey7056@sohamvc.org.uk>
2014-05-23 11:50:07 +0100

    Update grid.py
    Changed printing on python script page to turtle.

--- grid.py
+++ grid.py
@@ -1,4 +1,25 @@
-print("-------")
-print("| | | |")
-print("-------")
-print("| | | |")
+import time
+import turtle
+window = turtle.Screen()
+tim=turtle.Turtle()
+tim.speed(100)
+
+def grid():
+    for x in range (2):
+        tim.left(90)
+        tim.forward(100)
+        tim.left(90)
+        tim.forward(100)
+        for x in range(4):
+            tim.forward(100)
+            for x in range(4):
+                tim.right(90)
+                tim.forward(200)
+                for x in range(4):
+                    tim.right(90)
+                    tim.forward(100)
+                    
+
+                    
+
+
